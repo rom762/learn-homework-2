5546b963127507c1554bde5cbd8fc10e05d99788
nickr <nickro@ya.ru>
2021-02-21 18:33:57 +0300

    first is done

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -8,12 +8,19 @@
 
 """
 
+from datetime import datetime, timedelta
+import locale
+
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    today = datetime.today()
+    print(f'Вчера: {today - timedelta(days=1)}')
+    print(f'Сегодня: {today}')
+    print(f'Завтра: {today + timedelta(days=1)}')
+    print(f'30 дней назад: {today - timedelta(days=30)}')
 
 
 def str_2_datetime(date_string):
@@ -21,8 +28,11 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    str_format = '%d/%m/%y %H:%M:%S.%f'
+    return datetime.strptime(date_string, str_format)
+
 
 if __name__ == "__main__":
     print_days()
     print(str_2_datetime("01/01/20 12:10:03.234567"))
+
